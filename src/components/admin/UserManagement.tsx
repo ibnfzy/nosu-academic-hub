dfa446495a4ed4d9a6ba320c7d5b43b2867a2a8a
Ibnu Fauzy <58937373+ibnfzy@users.noreply.github.com>
2025-09-11 12:23:01 +0800

    change grid to sm

--- src/components/admin/UserManagement.tsx
+++ src/components/admin/UserManagement.tsx
@@ -347,7 +347,7 @@ export default function UserManagement({
                 </DialogTitle>
               </DialogHeader>
               <form onSubmit={handleUserSubmit} className="space-y-4">
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
                   <div className="space-y-2">
                     <Label>Username *</Label>
                     <Input
